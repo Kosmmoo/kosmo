b1a1da518e1e294600848072f1a5a7ea3e4cfb3a
Petr Novak <pronovaso@icloud.com>
2025-05-22 11:03:47 +0200

    refactor: 🔄 Clean up App and Header components
    * Removed unused logos and links from the `HeaderComponent`.
    * Simplified the structure of the `App` component.
    * Cleaned up CSS styles in `HeaderComponent.module.css` and `App.css`.
    * Enhanced readability and maintainability of the codebase.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 
 import './App.css';
-import reactLogo from './assets/react.svg';
 import {HeaderComponent} from './components/Header/HeaderComponent';
-import viteLogo from '/vite.svg';
 
 function App() {
   return (
     <>
-      <div>
-        <h1>Kosmo</h1>
-        <HeaderComponent />
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      <HeaderComponent />
+      <h1>Kosmo</h1>
     </>
   );
 }
--- src/components/Header/HeaderComponent.tsx
+++ src/components/Header/HeaderComponent.tsx
@@ -1,12 +1,15 @@
+import reactLogo from '../../assets/react.svg';
 import style from './HeaderComponent.module.css';
+import viteLogo from '/vite.svg';
 export const HeaderComponent = () => {
   return (
     <header className={style.header}>
-      <ul className={style.list}>
-        <li className={style.listItem}>O mně</li>
-        <li className={style.listItem}>Projekty</li>
-        <li className={style.listItem}>Kontakty</li>
-      </ul>
+      <a href="https://vite.dev" target="_blank">
+        <img src={viteLogo} className={style.logo} alt="Vite logo" />
+      </a>
+      <a href="https://react.dev" target="_blank">
+        <img src={reactLogo} className={style.logo} alt="React logo" />
+      </a>
     </header>
   );
 };
