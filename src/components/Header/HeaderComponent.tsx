9e54b11b15453730ab13f2afa73a35912b9536b6
Kosmo <vaclav.pesek2@educanet.cz>
2025-05-22 18:48:00 +0200

    feat: ✨ Add profile and footer components with styling
    * Updated `index.html` to include a new favicon and title.
    * Created `BodyComponent` and `FooterComponent` with respective styles.
    * Added profile image and logos for Discord and GitHub.
    * Refactored `HeaderComponent` to display social media links.
    * Adjusted global styles for better layout and appearance.

--- src/components/Header/HeaderComponent.tsx
+++ src/components/Header/HeaderComponent.tsx
@@ -1,12 +1,15 @@
+import discordLogo from '../../assets/discord_logo.png';
+import logoGithub from '../../assets/logo_github.png';
 import style from './HeaderComponent.module.css';
 export const HeaderComponent = () => {
   return (
     <header className={style.header}>
-      <ul className={style.list}>
-        <li className={style.listItem}>O mně</li>
-        <li className={style.listItem}>Projekty</li>
-        <li className={style.listItem}>Kontakty</li>
-      </ul>
+      <a href="https://discord.com/users/kosmo145" target="_blank">
+        <img src={discordLogo} className={style.logo} alt="discord" />
+      </a>
+      <a href="https://github.com" target="_blank">
+        <img src={logoGithub} className={style.logo} alt="github" />
+      </a>
     </header>
   );
 };
